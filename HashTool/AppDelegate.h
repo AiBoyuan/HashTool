0e99cba5dc925a4479be98a1a5061eb16fabbf67
Aibo <yuannb2008@126.com>
2019-01-29 11:15:00 +0800

    commit

---
+++ HashTool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HashTool
+//
+//  Created by Aibo on 2019/1/29.
+//  Copyright © 2019 Aibo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
